a6884a0efb3adc87616fde6274eb788e8f7e2306
aqutw <aquarian.ex+github@gmail.com>
2015-02-19 00:12:53 +0800

    FINISH fetch users documents

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://0.0.0.0/test002");
+mongoose.model('users', {name: String});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  mongoose.model('users').find(function(err, users){ 
+    res.send(users);
+  });
+  
+
 });
 
 module.exports = router;
